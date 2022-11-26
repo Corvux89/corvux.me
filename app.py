aba89655748688aa4718d6e6b42da296497655e1
Bryce Hadsell <bryce.hadsell@gmail.com>
2022-11-25 18:59:30 -0600

    Fixes: CSS margin fix to remove scrollbar for card-deck DB Conversion

--- app.py
+++ app.py
@@ -3,38 +3,24 @@ import json
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_talisman import Talisman
+from flask_sqlalchemy import SQLAlchemy
 
-from constants import WEB_DEBUG
+from constants import WEB_DEBUG, DB_URI
+from models import Character
 
 app = Flask(__name__)
 
 Bootstrap(app)
 
+db = SQLAlchemy()
+
+app.config["SQLALCHEMY_DATABASE_URI"] = DB_URI
+
+db.init_app(app)
+
 app.config.update(
     DEBUG=WEB_DEBUG
 )
-
-
-def setup_cards(file: str, link: str) -> str:
-    f = open(file, encoding="utf8")
-    data = f.read()
-    players = json.loads(data)
-
-    # Get cards ready
-    str = ""
-    for p in players.keys():
-        player = players[p]
-        str += "<div class=\"card rounded-4\">"
-        if player['image'] == "":
-            str += "<img class=\"card-img-top\" src=\"/static/images/Placeholder.png\">"
-        else:
-            str += "<img class=\"card-img-top\" src=\"/static/" + player['image'] + "\">"
-        str += "<div class=\"card-body\">"
-        str += "<h5 class=\"card-title\">" + player['name'] + "</h5>"
-        str += "<a href=\"" + link + p + "\" class=\"stretched-link\"></a></div></div>"
-
-    return str
-
 
 @app.route('/')
 @app.route('/home')
@@ -44,52 +30,46 @@ def homepage():
 
 @app.route('/Abeir-Toril_Walkabout/Era1')
 def atw_era_1():
-    players = setup_cards('data/era1players.json', '/Abeir-Toril_Walkabout/Era1/')
+    characters = db.session.execute(db.select(Character).filter_by(page=1).order_by(Character.order, Character.name)).scalars()
 
-    return render_template("game_main.html", players=players, header="Abeir-Toril Walkabout: Era 1")
+    return render_template("character_list.html", characters=characters, header="Abeir-Toril Walkabout: Era 1",
+                           link='/Abeir-Toril_Walkabout/Era1/')
 
 
-@app.route('/Abeir-Toril_Walkabout/Era1/<player>')
-def atw_era_1_player(player):
-    f = open('data/era1players.json', encoding="utf8")
-    data = f.read()
-    players = json.loads(data)
-    p = players[player]
-    return render_template("player.html", player=players[player],
+@app.route('/Abeir-Toril_Walkabout/Era1/<character>')
+def atw_era_1_character(character):
+    character = db.get_or_404(Character, character)
+    return render_template("character_profile.html", character=character,
                            button_caption="Back to Era 1", button_link="/Abeir-Toril_Walkabout/Era1")
 
 
 @app.route('/Abeir-Toril_Walkabout/Era2')
 def atw_era_2():
-    players = setup_cards('data/era2players.json', '/Abeir-Toril_Walkabout/Era2/')
+    characters = db.session.execute(db.select(Character).filter_by(page=2).order_by(Character.order, Character.name)).scalars()
 
-    return render_template("game_main.html", players=players, header="Abeir-Toril Walkabout: Era 2")
+    return render_template("character_list.html", characters=characters, header="Abeir-Toril Walkabout: Era 2",
+                           link='/Abeir-Toril_Walkabout/Era2/')
 
 
-@app.route('/Abeir-Toril_Walkabout/Era2/<player>')
-def atw_era_2_player(player):
-    f = open('data/era2players.json', encoding="utf8")
-    data = f.read()
-    players = json.loads(data)
-    p = players[player]
-    return render_template("player.html", player=players[player],
+@app.route('/Abeir-Toril_Walkabout/Era2/<character>')
+def atw_era_2_player(character):
+    character = db.get_or_404(Character, character)
+    return render_template("character_profile.html", character=character,
                            button_caption="Back to Era 2", button_link="/Abeir-Toril_Walkabout/Era2")
 
 
 @app.route('/Abeir-Toril_Walkabout/Saga3')
 def atw_saga_3():
-    players = setup_cards('data/saga3players.json', '/Abeir-Toril_Walkabout/Saga3/')
+    characters = db.session.execute(db.select(Character).filter_by(page=3).order_by(Character.order, Character.name)).scalars()
 
-    return render_template("game_main.html", players=players, header="Abeir-Toril Walkabout: Saga 3")
+    return render_template("character_list.html", characters=characters, header="Abeir-Toril Walkabout: Saga 3",
+                           link='/Abeir-Toril_Walkabout/Saga3/')
 
 
-@app.route('/Abeir-Toril_Walkabout/Saga3/<player>')
-def atw_saga_3_player(player):
-    f = open('data/saga3players.json', encoding="utf8")
-    data = f.read()
-    players = json.loads(data)
-    p = players[player]
-    return render_template("player.html", player=players[player],
+@app.route('/Abeir-Toril_Walkabout/Saga3/<character>')
+def atw_saga_3_player(character):
+    character = db.get_or_404(Character, character)
+    return render_template("character_profile.html", character=character,
                            button_caption="Back to Saga 3", button_link="/Abeir-Toril_Walkabout/Saga3")
 
 csp = {
@@ -108,11 +88,14 @@ csp = {
         'https://cdn.jsdelivr.net/',
         'https://www.googletagmanager.com/'
         ],
-    'img-src': '*',
+    'img-src': [
+        '*',
+        'data:'
+    ],
 
     'style-src': [
         '\'self\'',
-        'https://cdn.jsdelivr.net/'
+        'https://cdn.jsdelivr.net/',
     ]
 }
 
--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
 import os
 
 WEB_DEBUG = os.environ.get("DEBUG", False)
+
+DB_URI = os.environ.get("DATABASE_URI", "")

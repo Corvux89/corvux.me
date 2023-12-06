656fc49b445b841d5180899b20bc32daccf5a4ea
Bryce Hadsell <bryce.hadsell@gmail.com>
2023-12-06 10:59:32 -0600

    Calc updates for quick review

--- blueprints/sw5e_weapon_calculator.py
+++ blueprints/sw5e_weapon_calculator.py
@@ -6,10 +6,6 @@ weapon_calc = Blueprint("weap_calc",__name__)
 
 @weapon_calc.route('/')
 def calc():
-    return render_template('/SW5E_Weapon_Calculator/main.html')
-
-@weapon_calc.route('/Blasters')
-def blasters():
-    f = open('static/json/blasters.json', encoding="utf8")
+    f = open('static/json/sw5e_weapon_calculator.json', encoding="utf8")
     fields = json.load(f)
-    return render_template('/SW5E_Weapon_Calculator/calculator.html', fields=fields, title="Blasters")
+    return render_template('/SW5E_Weapon_Calculator/weapon_calculator.html', data=fields)
--- static/js/SW5E_Weapon_Calculator.js
+++ static/js/SW5E_Weapon_Calculator.js
@@ -1,12 +1,20 @@
 var weap_cal = JSON.parse(localStorage.getItem("SW5E_Weapons") || "{}")
 
-document.querySelectorAll('input, select').forEach(input =>{
-    input.addEventListener('input', calc_total)
+document.getElementById("blastersCalcForm").querySelectorAll('input, select').forEach(input =>{
+    input.addEventListener('input', function(){
+        calc_total('blasters')
+    })
 })
 
-function calc_total(){
-    var form = document.getElementById("calcForm")
-    var def_weight = weights.filter(obj => {return obj.points == 'default'})
+document.getElementById("lightweaponsCalcForm").querySelectorAll('input, select').forEach(input =>{
+    input.addEventListener('input', function(){
+        calc_total('lightweapons')
+    })
+})
+
+function calc_total(key){
+    var form = document.getElementById(`${key}CalcForm`)
+    var def_weight = reference[key].weights.filter(obj => {return obj.points == 'default'}) || weights.filter(obj => {return obj.points == '0'})
 
     var total = 0
     var cost = 0
@@ -22,7 +30,7 @@ function calc_total(){
     // Sum everything up
     form.querySelectorAll('select').forEach(input =>{
         var selection = input.options[input.selectedIndex]
-        var property = properties.filter(obj => {return obj.name == input.id})[0]
+        var property = reference[key].fields.filter(obj => {return obj.name == input.id.replace(`${key}-`,'')})[0]
 
         if (selection.getAttribute("point-mod")){
             point_mod = parseFloat(selection.getAttribute("point-mod"))
@@ -33,14 +41,14 @@ function calc_total(){
         // Cost
         if (selection.getAttribute("cost-override")){
             cost += parseFloat(selection.getAttribute("cost-override")) || 0
-        } else{
+        } else {
             cost += parseFloat(input.selectedIndex) * parseFloat(property.cost) || 0
         }
 
         // Weight
         if (selection.getAttribute("weight-override")){
             weight += parseFloat(selection.getAttribute("weight-override")) || 0
-        } else{
+        } else {
             weight += parseFloat(input.selectedIndex) * parseFloat(property.weight) || 0
         }
 
@@ -60,12 +68,12 @@ function calc_total(){
     var adj_total = Math.ceil(total)
 
     // Adjust based on points
-    for (var i = 0; i < weights.length; i++){
-        var w = weights[i]
+    for (var i = 0; i < reference[key].weights.length; i++){
+        var w = reference[key].weights[i]
         var points = parseInt(w.points)
 
         // Check if we are on the correct weight
-        if (!isNaN(points) && adj_total >= points && (i === (weights.length-1) || adj_total < parseFloat(weights[i+1].points))){
+        if (!isNaN(points) && adj_total >= points && (i === (reference[key].weights.length-1) || adj_total < parseFloat(reference[key].weights[i+1].points))){
             cost += parseFloat(w.cost) || 0
             weight += parseFloat(w.weight) || 0
             short = parseFloat(w.short) * range_mod
@@ -78,20 +86,41 @@ function calc_total(){
 
 
     // Some error validation
-    if (total > parseFloat(weights[weights.length-1].points) || adj_total <= -1){
+    if (total > parseFloat(reference[key].weights[reference[key].weights.length-1].points) || adj_total <= -1){
         die = "Invalid weapon"
         document.getElementById('summary-row').classList.add("bg-danger")
     } else if (document.getElementById('summary-row').classList.contains("bg-danger")) {
         document.getElementById('summary-row').classList.remove("bg-danger")
     }
 
-    document.getElementById('point-total').innerHTML = total
-    document.getElementById('damage-die').innerHTML = die || document.getElementById('damage-die').innerHTML
-    document.getElementById('cost-total').innerHTML = cost || document.getElementById('cost-total').innerHTML
-    document.getElementById('weight-total').innerHTML = weight || document.getElementById('weight-total').innerHTML
-    document.getElementById('short-range').innerHTML = short || document.getElementById('short-range').innerHTML
-    document.getElementById('long-range').innerHTML = long || document.getElementById('long-range').innerHTML
-    document.getElementById('reload').innerHTML = reload || document.getElementById('reload').innerHTML
+    if (document.getElementById(`${key}-point-total`) !== null){
+        document.getElementById(`${key}-point-total`).innerHTML = total
+    }
+
+    if (document.getElementById(`${key}-damage-die`) !== null){
+        document.getElementById(`${key}-damage-die`).innerHTML = die || document.getElementById(`${key}-damage-die`).innerHTML
+    }
+
+    if (document.getElementById(`${key}-cost-total`) !== null){
+        document.getElementById(`${key}-cost-total`).innerHTML = cost || document.getElementById(`${key}-cost-total`).innerHTML
+    }
+
+    if (document.getElementById(`${key}-weight-total`) !== null){
+        document.getElementById(`${key}-weight-total`).innerHTML = weight || document.getElementById(`${key}-weight-total`).innerHTML
+    }
+
+    if (document.getElementById(`${key}-short-range`) !== null){
+        document.getElementById(`${key}-short-range`).innerHTML = short || document.getElementById(`${key}-short-range`).innerHTML
+    }
+
+    if (document.getElementById(`${key}-long-range`) !== null){
+        document.getElementById(`${key}-long-range`).innerHTML = long || document.getElementById(`${key}-long-range`).innerHTML
+    }
+
+    if (document.getElementById(`${key}-reload`) !== null){
+        document.getElementById(`${key}-reload`).innerHTML = reload || document.getElementById(`${key}-reload`).innerHTML
+    }
 }
 
-calc_total()
+calc_total("blasters")
+calc_total("lightweapons")

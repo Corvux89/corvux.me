31f94d70b07c69e97d6d5abda625e7b0ed3edab5
Bryce Hadsell <bryce.hadsell@gmail.com>
2024-04-15 11:19:31 -0500

    Add DEV Route to test new Avrae Planner Live

--- constants.py
+++ constants.py
@@ -1,5 +1,4 @@
 import os
 
 WEB_DEBUG = os.environ.get("DEBUG", False)
-WEB_DEBUG = os.environ.get("DEBUG", True)
-SECRET_KEY = os.environ.get("SECRET_KEY", "1234")
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
